f1c882ed56b8515062c00ca51f29dbd5a40a5de2
Tim Williams <timwilliams.txW@gmail.com>
2019-01-15 01:40:14 +0000

    add headers to request

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,17 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ca4f3931a48fd695b3838a40bdf344168a3bd88e'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 
