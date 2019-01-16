9135bc6fdbdacd4473d0cdbe28dc0fa0c1b9a04b
Tim Williams <timwilliams.txW@gmail.com>
2019-01-16 01:38:14 +0000

    add url and path to array

--- download_avatars.js
+++ download_avatars.js
@@ -1,40 +1,47 @@
 var request = require('request');
 var fs = require('fs');
-var GITHUB_TOKEN = require('./secrets')
+var secrets = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
+var token = secrets.GITHUB_TOKEN;
+console.log("token: ", token)
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': GITHUB_TOKEN
+      'Authorization': token
     }
   };
-
   request(options, function(err, res, body) {
-    cb(err, body)
     var data = JSON.parse(body);
-    var url = [];
-    var filePath = [];
-    for (var i = 0; i < data.length; i++) {
-      url.push(data[i].avatar_url);
-      filePath.push('avatars/' + data[i].login);
-  } console.log(url, filePath)
-})
-}
+    var fileDownload = [];
+    for (var element in data) {
+      // var aviUrl = data[i].avatar_url
+      fileDownload.push ({
+        url:data[element].avatar_url,
+        path:'avatars/' + data[element].login
+      })
+      // console.log(data[element].login)
+      // fileDownload[element].path = 'avatars/' + data.login
 
+
+    } console.log(fileDownload);
+
+   cb(err, body)
+});
+}
 function downloadImageByURL(url, filePath) {
+  // console.log("download: ", url)
   request.get (url)
   .pipe(fs.createWriteStream(filePath));
 }
 
-//downloadImageByURL(aviUrl, path)
+// downloadImageByURL(url, filePath)
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   // console.log("Result:", result);
-
 });
+
